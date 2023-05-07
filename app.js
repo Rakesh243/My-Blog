8d90c0e1af8d3e1615c5943e9fec8246f5e4fb2b
Alan Miste <baranalaan@gmail.com>
2023-05-07 23:10:15 +0200

    Add create route.

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
+});
+
 app.use((req, res) => {
   res.status(404).render("404");
 });
