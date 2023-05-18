39829dc63fc973617f6d6aa6b079262cfe660f3f
Alan Miste <baranalaan@gmail.com>
2023-05-18 21:18:48 +0200

    Add DB_URI to .env file.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 
-const dbURI = "";
+const dbURI = process.env.DB_URI;
 
 mongoose
   .connect(dbURI)
