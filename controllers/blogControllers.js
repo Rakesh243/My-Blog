027b4326f4e92cb2370d74fe5e0d854e99fb168b
Alan Miste <baranalaan@gmail.com>
2023-05-18 20:45:18 +0200

    Create blogs folder in views folder.

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -4,7 +4,7 @@ const blogList = (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
+      res.render("blogs/index", { title: "All Blogs", blogs: result });
     })
     .catch((err) => console.log(err));
 };
@@ -13,13 +13,13 @@ const blogDetails = (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
     .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
+      res.render("blogs/details", { blog: result, title: "Blog Details" });
     })
     .catch((err) => console.log(err));
 };
 
 const blogCreateGet = (req, res) => {
-  res.render("create", { title: "Create new Blog" });
+  res.render("blogs/create", { title: "Create new Blog" });
 };
 
 const blogCreatePost = (req, res) => {
