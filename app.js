7cb1ebc40af7c1ad2fbe2fa2cd72cfcf8dc97212
Alan Miste <baranalaan@gmail.com>
2023-05-31 14:52:06 +0200

    Add auth routes.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const blogRoutes = require("./routes/blogRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -24,10 +25,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { title: "Àbout" });
+  res.render("about", { title: "About" });
 });
 
 app.use("/blogs", blogRoutes);
+
+app.use("/auth", authRoutes);
 
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
