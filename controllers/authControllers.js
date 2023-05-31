6abee70465b3604c0ded961e0a3dc2873f054893
Alan Miste <baranalaan@gmail.com>
2023-05-31 14:55:59 +0200

    Add userRegister controller.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,7 +1,11 @@
 const userLogin = (req, res) => {
   res.render("auth/login", { title: "Login" });
 };
+const userRegister = (req, res) => {
+  res.render("auth/register", { title: "Register" });
+};
 
 module.exports = {
   userLogin,
+  userRegister,
 };
