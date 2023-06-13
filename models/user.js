64ea2a3f02879c9e404faecc60955978dfef733d
Alan Miste <baranalaan@gmail.com>
2023-06-13 10:56:46 +0200

    Add valdiation.

--- models/user.js
+++ models/user.js
@@ -1,26 +1,22 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      minLength: 6,
-    },
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Please enter an E-mail."],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid E-mail."],
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    required: [true, "Please enter a password."],
+    minlength: [6, "Minimun password length is 6 characters."],
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
