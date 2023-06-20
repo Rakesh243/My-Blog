ff1b2aa8fed234d5da370a611c26e51c9af5b43f
Alan Miste <baranalaan@gmail.com>
2023-06-20 22:23:30 +0200

    Add handleErrors.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,17 @@
-const bcrypt = require("bcrypt");
 const User = require("../models/User");
+
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
 
 const loginGet = (req, res) => {
   res.render("auth/login", { title: "Login" });
@@ -12,17 +24,14 @@ const registerGet = (req, res) => {
 };
 
 const registerPost = async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = new User(req.body);
-    user.password = hashedPassword;
+  const { email, password } = req.body;
 
-    user
-      .save()
-      .then((result) => res.redirect("/auth/login"))
-      .catch((err) => console.log(err));
-  } catch {
-    res.redirect("/auth/register");
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
 
