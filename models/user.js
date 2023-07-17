9bf9d4089861d0d8c6d433a3a0602d7b1ae738de
Alan Miste <baranalaan@gmail.com>
2023-07-17 20:33:32 +0200

    Display errors in frontend.

--- models/user.js
+++ models/user.js
@@ -34,9 +34,9 @@ userSchema.statics.login = async function (email, password) {
     if (auth) {
       return user;
     }
-    throw Error("Incorrect password.");
+    throw Error("Incorrect password");
   }
-  throw Error("Incorrect Email.");
+  throw Error("Incorrect Email");
 };
 
 const User = mongoose.model("user", userSchema);
