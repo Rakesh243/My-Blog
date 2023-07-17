96ef205183f8f262e1b53bd5c1881c8d4b5a8edb
Alan Miste <baranalaan@gmail.com>
2023-07-18 00:05:34 +0200

    Protect create new blog route.

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const blogController = require("../controllers/blogControllers");
+const { requireAuth } = require("../middleware/authMiddleware");
 
 router.get("/", blogController.blogList);
 
 router.post("/", blogController.blogCreatePost);
 
-router.get("/create", blogController.blogCreateGet);
+router.get("/create", requireAuth, blogController.blogCreateGet);
 
 router.get("/:id", blogController.blogDetails);
 
